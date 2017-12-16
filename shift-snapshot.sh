e988c3ca044aa314683db75ce42fa1012fb7e5cb
biolypl <terazbitcoin@gmail.com>
2017-12-16 14:10:27 +0100

    snaphost format working with shift_manager.sh restore, delete useless functions

--- shift-snapshot.sh
+++ shift-snapshot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-VERSION="0.2"
+VERSION="0.2.1"
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 #============================================================
-#= snapshot.sh v0.2 created by mrgr                         =
-#= Please consider voting for delegate mrgr                 =
+#= snapshot.sh v0.2 created by mrgr
+#= edited by bioly
+#= Please consider voting for delegate mrgr and bioly ;)
 #============================================================
 echo " "
 
@@ -23,72 +24,34 @@ fi
 
 SHIFT_CONFIG=~/shift/config.json
 DB_NAME="$(grep "database" $SHIFT_CONFIG | cut -f 4 -d '"')"
-DB_USER="$(grep "user" $SHIFT_CONFIG | cut -f 4 -d '"')"
-DB_PASS="$(grep "password" $SHIFT_CONFIG | cut -f 4 -d '"' | head -1)"
-SNAPSHOT_COUNTER=snapshot/counter.json
-SNAPSHOT_LOG=snapshot/snapshot.log
-if [ ! -f "snapshot/counter.json" ]; then
-  mkdir -p snapshot
-  sudo chmod a+x shift-snapshot.sh
-  echo "0" > $SNAPSHOT_COUNTER
-  sudo chown postgres:${USER:=$(/usr/bin/id -run)} snapshot
-  sudo chmod -R 777 snapshot
-fi
+SNAPSHOT_LOG=snapshot.log
 SNAPSHOT_DIRECTORY=snapshot/
+if [ ! -f $SNAPSHOT_DIRECTORY ]; then
+ mkdir -p $SNAPSHOT_DIRECTORY
+fi
 
 
 NOW=$(date +"%d-%m-%Y - %T")
 ################################################################################
 
 create_snapshot() {
-  export PGPASSWORD=$DB_PASS
   echo " + Creating snapshot"
   echo "--------------------------------------------------"
   echo "..."
-  sudo su postgres -c "pg_dump -Ft $DB_NAME > $SNAPSHOT_DIRECTORY'shift_db$NOW.snapshot.tar'"
-  blockHeight=`psql -d $DB_NAME -U $DB_USER -h localhost -p 5432 -t -c "select height from blocks order by height desc limit 1;"`
-  dbSize=`psql -d $DB_NAME -U $DB_USER -h localhost -p 5432 -t -c "select pg_size_pretty(pg_database_size('$DB_NAME'));"`
-
+  blockHeight="$(curl -k -X GET http://localhost:9305/api/blocks/getHeight | jq '.height')"
+  fullPath="${SNAPSHOT_DIRECTORY}blockchain.db_${blockHeight}.gz"
+  fullPathLast="${SNAPSHOT_DIRECTORY}blockchain.db.gz"
+  sudo -u postgres pg_dump -O "$DB_NAME" | gzip > "$fullPath"
   if [ $? != 0 ]; then
     echo "X Failed to create snapshot." | tee -a $SNAPSHOT_LOG
     exit 1
   else
-    echo "$NOW -- OK snapshot created successfully at block$blockHeight ($dbSize)." | tee -a $SNAPSHOT_LOG
+    echo "$NOW -- OK snapshot $fullPath created successfully" | tee -a $SNAPSHOT_LOG
+    sudo cp $fullPath $fullPathLast
   fi
 
 }
 
-restore_snapshot(){
-  echo " + Restoring snapshot"
-  echo "--------------------------------------------------"
-  SNAPSHOT_FILE=`ls -t snapshot/shift_db* | head  -1`
-  if [ -z "$SNAPSHOT_FILE" ]; then
-    echo "****** No snapshot to restore, please consider create it first"
-    echo " "
-    exit 1
-  fi
-  echo "Snapshot to restore = $SNAPSHOT_FILE"
-
-  read -p "Please stop node app.js first, are you ready (y/n)? " -n 1 -r
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
-     echo "***** Please stop node.js first.. then execute restore again"
-     echo " "
-     exit 1
-  fi
-
-#snapshot restoring..
-  export PGPASSWORD=$DB_PASS
-  pg_restore -d $DB_NAME "$SNAPSHOT_FILE" -U $DB_USER -h localhost -c -n public
-
-  if [ $? != 0 ]; then
-    echo "X Failed to restore."
-    exit 1
-  else
-    echo "OK snapshot restored successfully."
-  fi
-
-}
 
 show_log(){
   echo " + Snapshot Log"
@@ -103,9 +66,6 @@ case $1 in
 "create")
   create_snapshot
   ;;
-"restore")
-  restore_snapshot
-  ;;
 "log")
   show_log
   ;;
@@ -115,13 +75,12 @@ case $1 in
 "help")
   echo "Available commands are: "
   echo "  create   - Create new snapshot"
-  echo "  restore  - Restore the last snapshot available in folder snapshot/"
   echo "  log      - Display log"
   ;;
 *)
   echo "Error: Unrecognized command."
   echo ""
-  echo "Available commands are: create, restore, log, help"
+  echo "Available commands are: create, log, help"
   echo "Try: bash shift-snapshot.sh help"
   ;;
 esac
